7474a6cc64c39a268c3fc75f25792fe8de966af3
Nanit20 <55482552+Nanit20@users.noreply.github.com>
2024-04-17 15:54:06 +0000

    first commit

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<div className=" position-absolute bottom-0 col-12 footercontainer text-center bg-dark text-white">
+            <p> Made by William </p>
+
+		</div>
+	);
+};
+
+export default Footer;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,26 @@
 import React from "react";
-
+import {Navbar} from "./nav.jsx"
+import {Jumbotron} from "./jumbotron.jsx"
+import {Cards} from "./cards.jsx"
+import Footer from "./footer.jsx"
+import "../../styles/index.css"
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="pb-5 position-relative"style={{height:"100vh",}}>
+			<Navbar/>
+			<Jumbotron/>
+
+			<div className="container" style={{display:"flex", justifyContent: "space-around"}}>
+				<Cards/> 
+				<Cards/> 
+				<Cards/> 
+				<Cards/>
+			</div>
+			<Footer/>
 		</div>
 	);
 };
